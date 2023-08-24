c3c234816aae8b31e3ac368af94e3c87bf6b854c
Filipe Belo <filipeabelo@gmail.com>
2023-08-24 14:06:39 -0300

    InfoWidget layout fix

--- lib/commons/components/info_widget.dart
+++ lib/commons/components/info_widget.dart
@@ -10,31 +10,24 @@ class InfoWidget extends StatelessWidget {
   final String title;
   final String description;
   final Icon icon;
-
+  
   @override
   Widget build(BuildContext context) {
-    final textTheme = Theme.of(context)
-        .textTheme
-        .apply(displayColor: Theme.of(context).colorScheme.onSurface);
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        icon,
-        Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            Text(
-              title,
-              style: textTheme.titleMedium,
-              textAlign: TextAlign.start,
-            ),
-            Text(
-              description,
-              style: textTheme.bodyMedium,
-            )
-          ],
-        )
-      ],
-    );
+    final textTheme = Theme.of(context).textTheme;
+    return Column(children: [
+      Row(
+        children: [
+          icon,
+          Text(
+            title,
+            style: textTheme.titleMedium,
+          )
+        ],
+      ),
+      Text(
+        description,
+        style: textTheme.bodyMedium,
+      )
+    ]);
   }
 }
