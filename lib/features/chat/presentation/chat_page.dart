580b8f67cfb58e14a166e86f6b63dd62caa49d0a
Filipe Belo <filipeabelo@gmail.com>
2023-09-06 14:52:07 -0300

    Create initial chat style page

--- lib/features/chat/presentation/chat_page.dart
+++ lib/features/chat/presentation/chat_page.dart
@@ -3,31 +3,51 @@ import 'package:chat_gpt/features/chat/presentation/cubit/chat_cubit.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-class ChatPage extends StatelessWidget {
+class ChatPage extends StatefulWidget {
   const ChatPage({super.key});
+
+  @override
+  State<ChatPage> createState() => _ChatPageState();
+}
+
+class _ChatPageState extends State<ChatPage> {
+  final textController = TextEditingController();
+  final _messages = List<String>.empty(growable: true);
+
+  @override
+  void dispose() {
+    textController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return BlocProvider(
-      create: (context) => ChatCubit(service: ChatServiceImpl())..sendMessage(),
+      create: (context) => ChatCubit(service: ChatServiceImpl()),
       child: Scaffold(
-        body: Center(
-            child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+        body: Column(
           children: [
-            BlocBuilder<ChatCubit, ChatState>(
-              builder: (context, state) {
-                switch (state.runtimeType) {
-                  case ChatResult:
-                    state as ChatResult;
-                    return Text(state.chatResult.choices[0].message.content);
-                  default:
-                    return const Text('Initial State');
-                }
-              },
+            TextField(
+              controller: textController,
             ),
+            Expanded(
+              child: ListView.builder(
+                  itemCount: _messages.length,
+                  itemBuilder: (context, index) {
+                    final item = _messages[index];
+
+                    return Text(item);
+                  }),
+            )
           ],
-        )),
+        ),
+        floatingActionButton: FloatingActionButton(
+            child: const Text('oi'),
+            onPressed: () {
+              setState(() {
+                _messages.add(textController.text);
+              });
+            }),
       ),
     );
   }
