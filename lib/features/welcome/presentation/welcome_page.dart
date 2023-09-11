27cdb81be4f83c3e9936f70584d8785bc212792a
Filipe Belo <filipeabelo@gmail.com>
2023-09-10 21:23:10 -0300

    Add scroll to Welcome Page

--- lib/features/welcome/presentation/welcome_page.dart
+++ lib/features/welcome/presentation/welcome_page.dart
@@ -12,58 +12,70 @@ class MyHomePage extends StatelessWidget {
     return Scaffold(
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: Column(
-          children: [
-            Column(
-              children: [
-                Text(
-                  pageTitle,
-                  style: textTheme.headlineLarge,
+        child: LayoutBuilder(builder:
+            (BuildContext context, BoxConstraints viewportConstraints) {
+          return SingleChildScrollView(
+            child: ConstrainedBox(
+              constraints: BoxConstraints(
+                minHeight: viewportConstraints.maxHeight,
+              ),
+              child: IntrinsicHeight(
+                child: Column(
+                  children: [
+                    Text(
+                      pageTitle,
+                      style: textTheme.headlineLarge,
+                    ),
+                    Container(
+                      margin: const EdgeInsets.only(top: 27),
+                      child: Text(
+                        pageDescription,
+                        style: textTheme.bodyMedium,
+                      ),
+                    ),
+                    Container(
+                      margin: const EdgeInsets.only(top: 30),
+                      child: const InfoWidget(
+                          title: infoTitle1,
+                          description: infoDescription1,
+                          icon: ImageIcon(AssetImage('icons/globe.png'),
+                              color: Color(0xff1DC27D))),
+                    ),
+                    Container(
+                      margin: const EdgeInsets.only(top: 30),
+                      child: const InfoWidget(
+                          title: infoTitle2,
+                          description: infoDescription2,
+                          icon: ImageIcon(AssetImage('icons/lock.png'),
+                              color: Color(0xff8783FF))),
+                    ),
+                    Container(
+                      margin: const EdgeInsets.only(top: 30),
+                      child: const InfoWidget(
+                          title: infoTitle3,
+                          description: infoDescription3,
+                          icon: ImageIcon(AssetImage('icons/sliders.png'),
+                              color: Color(0xff000000))),
+                    ),
+                    Expanded(child: Container()),
+                    Container(
+                      margin: const EdgeInsets.only(top: 30),
+                      child: FilledButton.tonal(
+                          style: FilledButton.styleFrom(
+                              minimumSize: const Size.fromHeight(40)),
+                          onPressed: () => context.pushNamed(routerChat),
+                          child: Text(
+                            buttonText,
+                            style: textTheme.bodyMedium?.copyWith(
+                                fontSize: 14, color: const Color(0xff343345)),
+                          )),
+                    ),
+                  ],
                 ),
-                Container(
-                  margin: const EdgeInsets.only(top: 27),
-                  child: Text(
-                    pageDescription,
-                    style: textTheme.bodyMedium,
-                  ),
-                ),
-                Container(
-                  margin: const EdgeInsets.only(top: 30),
-                  child: const InfoWidget(
-                      title: infoTitle1,
-                      description: infoDescription1,
-                      icon: ImageIcon(AssetImage('icons/globe.png'),
-                          color: Color(0xff1DC27D))),
-                ),
-                Container(
-                  margin: const EdgeInsets.only(top: 30),
-                  child: const InfoWidget(
-                      title: infoTitle2,
-                      description: infoDescription2,
-                      icon: ImageIcon(AssetImage('icons/lock.png'),
-                          color: Color(0xff8783FF))),
-                ),
-                Container(
-                  margin: const EdgeInsets.only(top: 30),
-                  child: const InfoWidget(
-                      title: infoTitle3,
-                      description: infoDescription3,
-                      icon: ImageIcon(AssetImage('icons/sliders.png'),
-                          color: Color(0xff000000))),
-                ),
-              ],
+              ),
             ),
-            FilledButton.tonal(
-                style: FilledButton.styleFrom(
-                    minimumSize: const Size.fromHeight(40)),
-                onPressed: () => context.pushNamed(routerChat),
-                child: Text(
-                  buttonText,
-                  style: textTheme.bodyMedium
-                      ?.copyWith(fontSize: 14, color: const Color(0xff343345)),
-                )),
-          ],
-        ),
+          );
+        }),
       ),
     );
   }
