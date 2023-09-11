6df9598b787202aaea6baed6a9318e87ad512242
Filipe Belo <filipeabelo@gmail.com>
2023-09-11 16:42:07 -0300

    Chat page input and default state layout

--- lib/features/chat/presentation/chat_page.dart
+++ lib/features/chat/presentation/chat_page.dart
@@ -27,15 +27,13 @@ class _ChatPageState extends State<ChatPage> {
         return Scaffold(
           body: Column(
             children: [
-              TextField(
-                controller: textController,
-              ),
               Expanded(
                 child: BlocBuilder<ChatCubit, ChatState>(
                   builder: (context, state) {
                     switch (state) {
                       case ChatInitial():
-                        return const Text('Say something to me!');
+                        return const Image(
+                            image: AssetImage('images/gpt_logo.png'));
                       case ChatResult():
                         return ListView.builder(
                             itemCount: state.messages.length,
@@ -49,15 +47,41 @@ class _ChatPageState extends State<ChatPage> {
                     }
                   },
                 ),
-              )
+              ),
+              Container(
+                margin: const EdgeInsets.only(right: 16, left: 16, bottom: 12),
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: TextField(
+                        controller: textController,
+                        decoration: InputDecoration(
+                            prefixIcon: const Icon(Icons.mic),
+                            hintText: 'Message',
+                            border: OutlineInputBorder(
+                                borderSide: BorderSide.none,
+                                borderRadius: BorderRadius.circular(25)),
+                            filled: true,
+                            fillColor:
+                                Theme.of(context).colorScheme.primaryContainer),
+                      ),
+                    ),
+                    const SizedBox(
+                      width: 14,
+                    ),
+                    IconButton.filledTonal(
+                        onPressed: () {
+                          context
+                              .read<ChatCubit>()
+                              .sendMessage(textController.text);
+                          textController.value = TextEditingValue.empty;
+                        },
+                        icon: const Icon(Icons.arrow_upward))
+                  ],
+                ),
+              ),
             ],
           ),
-          floatingActionButton: FloatingActionButton(
-              child: const Text('oi'),
-              onPressed: () {
-                context.read<ChatCubit>().sendMessage(textController.text);
-                textController.value = TextEditingValue.empty;
-              }),
         );
       }),
     );
--- lib/main.dart
+++ lib/main.dart
@@ -13,25 +13,28 @@ class MyApp extends StatelessWidget {
     return MaterialApp.router(
       title: 'ChatGPT',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: const Color(0xffE8EBFE)),
-        useMaterial3: true,
-        fontFamily: 'Inter',
-        textTheme: const TextTheme(
-          headlineLarge: TextStyle(
-              fontSize: 45,
-              fontWeight: FontWeight.w500,
-              color: Color(0xff000000)),
-          bodyMedium: TextStyle(
+          colorScheme: ColorScheme.fromSeed(seedColor: const Color(0xffE8EBFE)),
+          useMaterial3: true,
+          fontFamily: 'Inter',
+          textTheme: const TextTheme(
+            headlineLarge: TextStyle(
+                fontSize: 45,
+                fontWeight: FontWeight.w500,
+                color: Color(0xff000000)),
+            bodyMedium: TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.w400,
+                color: Color(0xff716C72)),
+            titleMedium: TextStyle(
               fontSize: 16,
-              fontWeight: FontWeight.w400,
-              color: Color(0xff716C72)),
-          titleMedium: TextStyle(
-            fontSize: 16,
-            fontWeight: FontWeight.w500,
-            color: Color(0xff716C72),
+              fontWeight: FontWeight.w500,
+              color: Color(0xff716C72),
+            ),
           ),
-        ),
-      ),
+          iconButtonTheme: IconButtonThemeData(
+              style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all<Color>(
+                      const Color(0xffECE6EA))))),
       routerConfig: router,
     );
   }
